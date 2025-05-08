76073e852ee950f226adee2f0ed7348ef9a45be7
heeyong1203 <rhyong1203@gmail.com>
2025-05-08 16:35:46 +0900

    금일 Java, JavaScript 수업 종료

--- front_worspace/html_workspace/JS/day0507/js/Rect.js
+++ front_worspace/html_workspace/JS/day0507/js/Rect.js
@@ -16,7 +16,7 @@ class Rect{
         this.div.style.left=this.x+"px";
         this.div.style.top=this.y+"px";
         this.div.style.width=this.width+"px";
-        // this.div.style.height=this.height+"px";
+        this.div.style.height=this.height+"px";
         this.div.style.background=this.bg;
     
         this.container.appendChild(this.div); // 부모에 부착
@@ -28,7 +28,8 @@ class Rect{
         console.log("move()...");
         // 여기서 막대의 크기를 감속도 공식을 적용하여 움직여보자
         // 높이 = 초기높이 + 계수 * (목표높이-초기높이)
-        this.div.style.height = parseFloat((this.height)) + this.a*(parseFloat(this.targetH-this.height))+"px"
+        this.height = parseInt((this.height)) + this.a*(parseInt(this.targetH-this.height))+"px"
+        
         setTimeout(()=>{
             this.move();
         }, 10);
