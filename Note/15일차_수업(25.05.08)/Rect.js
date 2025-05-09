08f2c2b71243b66eab83fa03bce4f239bba48975
heeyong1203 <rhyong1203@gmail.com>
2025-05-09 09:44:49 +0900

    날짜객체연습 newDate 활용법 숙지

--- Note/15일차_수업(25.05.08)/Rect.js
+++ Note/15일차_수업(25.05.08)/Rect.js
@@ -1,4 +1,3 @@
-/* 현실의 사각형 막대를 정의한다. */
 class Rect{
     constructor(container, x, y, width, height, bg){
         this.container=container;
@@ -8,10 +7,9 @@ class Rect{
         this.width=width;
         this.height=height;
         this.bg=bg;
-        this.a=0.1;
-        this.targetH=400;
-        this.count=0;
-
+        this.targetH=200;
+        this.a=0.05;
+        
         //style 부여
         this.div.style.position="absolute";
         this.div.style.left=this.x+"px";
@@ -19,25 +17,20 @@ class Rect{
 
         this.div.style.width=this.width+"px";
         this.div.style.height=this.height+"px";
+        this.div.style.backgroundColor=this.bg;
 
-        this.div.style.background=this.bg;
-    
-        this.container.appendChild(this.div); // 부모에 부착
+        this.container.appendChild(this.div);
 
-        this.move(); // 태어나자마자, 나의 루프 호출
+        this.move();
     }
 
     move(){
-        console.log("move()...");
-        // 여기서 막대의 크기를 감속도 공식을 적용하여 움직여보자
-        // 높이 = 초기높이 + 계수 * (목표높이-초기높이)
-        this.height = parseFloat((this.height)) + this.a*(this.targetH-parseFloat(this.height));
-        
+        this.height=parseFloat(this.height)+this.a*(this.targetH-parseFloat(this.height));
+
         this.div.style.height=this.height+"px";
         setTimeout(()=>{
             this.move();
         }, 10);
     }
 
-   
 }
