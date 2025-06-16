fcbe323b91ecb8ddbbe620c8709f0cfaeb7e069d
(Heeyong Ryu) <(rhyong1203@gmail.com)>
2025-06-16 20:13:41 +0900

    remove unrelated folders for db branch

--- front_workspace/html_workspace/JS/미로게임/Brick.js
+++
@@ -1,7 +0,0 @@
-class Brick extends GameObject{
-    constructor(container, x, y, width, height, velX, velY, bg, border, borderColor){
-        // js는 java처럼 super()자동 호출해주는 기능이 없으므로, 개발자가 무조건 직접 나서야 함
-        super(container, x, y, width, height, velX, velY, bg, border, borderColor);
-        
-    }
-}
